dafef8bdc432d7947b061f1a5197834e002a215f
Marcos Acosta <marcos@acosta.tv>
2012-04-09 21:06:35 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2'
+
+gem 'pg'
+gem 'slim-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'jbuilder'
+gem 'devise'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'heroku'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaterVideo::Application.config.secret_token = '4dffc1d641652d7a06d4e4f0a4c2dd02f4d48cc967ed761eb1f395bdc72a99ee88e0907505851560e7faecfbc9286bc2a6bf15da8b0eecd5a68af07770990bcd'
